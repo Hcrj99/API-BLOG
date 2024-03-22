fde0687a3d4a4ebfd011aa2ec9918755299e780e
hcrj99 <hcrj99@hotmail.com>
2024-03-21 20:12:28 -0500

    add initial struct project

--- index.js
+++ index.js
@@ -23,11 +23,30 @@ app.use(express.json());//convert all in object js
 
 
 //create rutes
+app.get("/test", (req, res) => {
+
+    console.log("start end point test");
+
+
+    return res.status(200).json([
+        {
+            curse: "react",
+            name: "camilo",
+            url: "camiloDEV.com"
+        },
+        {
+            curse: "react",
+            name: "camilo",
+            url: "camiloDEV.com"
+        }
+    ])
+
+});//rute for get -navegator
 
 
 //create server
 app.listen(port, () => {
-    console.log("server run in port"+port);
+    console.log("server run in port" + "" + port);
 });//port to listen
 
 
